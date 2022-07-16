0cfd657f1b21b5678d71c830c5ce9c7a9367f7bc
Bunny Blake <discolingua@gmail.com>
2022-07-15 23:36:17 -0400

    gem pulling

--- Scripts/PickupGem.gd
+++ Scripts/PickupGem.gd
@@ -7,5 +7,5 @@ onready var tween = get_node("Tween")
 func _on_PickupGem_body_entered(body:Node):
 	if body.name == "Player":
 		tween.interpolate_property(self, "position", self.position, body.position,
-			1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 		tween.start()
